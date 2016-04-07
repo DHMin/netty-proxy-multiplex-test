41683da7af89802fe3ba5bd5897afbc7d8b1310b
DHMin <hyuki0920@gmail.com>
2016-04-08 01:04:48 +0900

    Front server of single connect test

---
+++ single-connect/src/main/java/com/dhmin/test/netty/proxy/multiplexing/singleconnect/ProxyConnector.java
@@ -0,0 +1,67 @@
+package com.dhmin.test.netty.proxy.multiplexing.singleconnect;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+public class ProxyConnector {
+	private static final Logger log = LoggerFactory.getLogger(SingleConnectServer.class);
+
+	private static final String BACKEND_ADDR = "127.0.0.1";
+	private static final int BACKEND_PORT = 21000;
+
+	private ConcurrentMap<Integer, Channel> channelHashCodeMap = new ConcurrentHashMap<>();
+
+	private Channel proxyChannel;
+
+	public ProxyConnector() {
+		EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+		Bootstrap b = new Bootstrap();
+		b.group(eventLoopGroup)
+		 .channel(NioSocketChannel.class)
+		 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+		 .option(ChannelOption.TCP_NODELAY, true)
+		 .option(ChannelOption.SO_KEEPALIVE, true)
+		 .handler(new ProxyHandler());
+
+		b.connect(BACKEND_ADDR, BACKEND_PORT).addListener((ChannelFuture future) -> {
+			if (future.isSuccess()) {
+				proxyChannel = future.channel();
+				log.info("PROXY CONNECTED: {}", proxyChannel);
+			}
+		});
+	}
+
+	public Channel getProxyChannel(Channel frontChannel) {
+		channelHashCodeMap.put(frontChannel.hashCode(), frontChannel);
+		return proxyChannel;
+	}
+
+	public void removeChannel(Channel frontChannel) {
+		channelHashCodeMap.remove(frontChannel.hashCode());
+	}
+
+	class ProxyHandler extends ChannelDuplexHandler {
+		@Override
+		public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+			ByteBuf in = (ByteBuf) msg;
+			int channelHashCode = in.readInt();
+			channelHashCodeMap.get(channelHashCode).writeAndFlush(in);
+			ReferenceCountUtil.safeRelease(in);
+		}
+	}
+}
---
+++ single-connect/src/main/java/com/dhmin/test/netty/proxy/multiplexing/singleconnect/SingleConnectServerHandler.java
@@ -0,0 +1,47 @@
+package com.dhmin.test.netty.proxy.multiplexing.singleconnect;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author DHMin
+ */
+public class SingleConnectServerHandler extends ChannelInboundHandlerAdapter {
+	private static final Logger log = LoggerFactory.getLogger(SingleConnectServerHandler.class);
+
+	private Channel proxyChannel;
+	private ProxyConnector proxyConnector;
+
+	public SingleConnectServerHandler(ProxyConnector proxyConnector) {
+		this.proxyConnector = proxyConnector;
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		log.info("ACTIVE: {}", ctx.channel());
+		proxyChannel = proxyConnector.getProxyChannel(ctx.channel());
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		log.info("INACTIVE: {}", ctx.channel());
+		proxyConnector.removeChannel(ctx.channel());
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf in = (ByteBuf) msg;
+		log.info("{} READ: {}", ctx.channel(), in);
+		ByteBuf out = ctx.alloc().buffer();
+		out.writeInt(ctx.channel().hashCode());
+		in.readBytes(out);
+		ReferenceCountUtil.safeRelease(in);
+		proxyChannel.writeAndFlush(out);
+	}
+}
